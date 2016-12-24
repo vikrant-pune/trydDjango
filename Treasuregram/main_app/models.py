4120dfea21259c639b019b661fd81fed19da621f
Vikrant Pawar <pawarv@us.ibm.com>
2016-12-24 14:48:47 +0530

    Adding data

--- Treasuregram/main_app/models.py
+++ Treasuregram/main_app/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Treasure(models.Model):
+	name = models.CharField(max_length=100)
+	value = models.DecimalField(max_digits=10, decimal_places=2)
+	material = models.CharField(max_length=20)
+	location = models.CharField(max_length=100)
